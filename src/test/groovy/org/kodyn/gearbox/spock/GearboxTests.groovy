1ed2f0cf5a6420efd2f45ff1f27f3f89f76de25e
Karol Jung <jungkarol@gmail.com>
2020-05-06 17:37:26 +0200

    Update test

--- src/test/groovy/org/kodyn/gearbox/spock/GearboxTests.groovy
+++ src/test/groovy/org/kodyn/gearbox/spock/GearboxTests.groovy
@@ -1,23 +1,25 @@
 package org.kodyn.gearbox.spock
 
 import org.kodyn.gearbox.GearboxDriver
+import org.kodyn.gearbox.Threshold
 import org.springframework.beans.factory.annotation.Autowired
 import spock.lang.Specification
 
 
 class GearboxTests extends Specification {
 
-    @Autowired
     GearboxDriver gearboxDriver;
 
     def setupSpec() {
     }
 
     def "when handleGas" () {
-        int beforeHandleGas = gearboxDriver.getCurrentGear()
-        when handleGas()
-        int afterHandleGas = gearboxDriver.getCurrentGear()
-        than
-        assert beforeHandleGas >= afterHandleGas
+        given:
+            int beforeHandleGas = gearboxDriver.getCurrentGear()
+        when:
+            gearboxDriver.handleGas(new Threshold(5d))
+            int afterHandleGas = gearboxDriver.getCurrentGear()
+        than:
+            beforeHandleGas >= afterHandleGas
     }
 }
